365ff85404973f77bfc5140262b796f5f4e4955f
Jacob <jacobpchacalco@gmail.com>
2020-11-26 16:56:47 -0800

    Added a <GenerateProgramFile>false</GenerateProgramFile> in the project, implemented InventoryTest and CharacterTest unitTests

--- Assignment5/Assignment5/CharacterTest.cs
+++ Assignment5/Assignment5/CharacterTest.cs
@@ -6,7 +6,59 @@ using NUnit.Framework;
 
 namespace Assignment5
 {
+    [TestFixture]
     public class CharacterTest
     {
+        // Our test character
+        private Character testCharacter;
+
+        [SetUp]
+        public void SetUp()
+        {
+            testCharacter = new Character("JohnnyTest", RaceCategory.Human, 300);
+        }
+
+        [TearDown]
+        public void CleanUp()
+        {
+            testCharacter = null;
+        }
+
+        [TestCase(100)]
+        public void TakeDamageReduceCorrectAmountHealth(int finalHealth)
+        {
+            int damage = 200;
+            testCharacter.TakeDamage(damage);
+
+            // Check if the testCharacter health is equal to 100 after taking 200 hitpoints damage
+            Assert.AreEqual(testCharacter.mHealth, finalHealth);
+        }
+
+        [TestCase(400)]
+        public void TakeDamageSetAlivetoFalse(int damage)
+        {
+            testCharacter.TakeDamage(damage);
+
+            // Check if the testCharacter isAlive bool is set to false after taking 400 hitpoints damage
+            Assert.IsFalse(testCharacter.isAlive);
+        }
+
+        [TestCase(500)]
+        public void RestoreHealthRestoreCorrectAmountHealth(int heal)
+        {
+            int recoveredHealth = 200;
+            testCharacter.RestoreHealth(recoveredHealth);
+
+            // Check if the testCharacter's health is equal to 500 after recovering 200 healpoints
+            Assert.AreEqual(testCharacter.mHealth, heal);
+        }
+
+        [Test]
+        public void RestoreHealthSetAlivetoTrue()
+        {
+            // Check if the testCharacter isAlive bool is set to true when his health is above 0
+            Assert.IsTrue(testCharacter.isAlive);
+        }
+
     }
 }
--- Assignment5/Assignment5/InventoryTest.cs
+++ Assignment5/Assignment5/InventoryTest.cs
@@ -6,7 +6,88 @@ using NUnit.Framework;
 
 namespace Assignment5
 {
+    [TestFixture]
     public class InventoryTest
     {
+        // Create our testInventory
+        private Inventory testInventory;
+
+        // Create a testItem
+        private Item testItem;
+
+        [SetUp]
+        public void SetUp()
+        {
+            testInventory = new Inventory(20);
+            testItem = new Item("Ancient Key", 300, ItemGroup.Key);
+        }
+
+        [TearDown]
+        public void CleanUp()
+        {
+            testInventory.Reset();
+            testInventory = null;
+
+            testItem = null;
+        }
+
+        [TestCase(18)]
+        public void RemoveItemAvailableSlotsIncrease(int finalSlots)
+        {
+            for(int x = 0; x < 3; x++)
+            {
+                testInventory.AddItem(testItem);
+            }
+
+            testInventory.TakeItem("Ancient Key", out Item item);
+
+            // Check if the availableSlots changed at the end
+            Assert.AreEqual(testInventory.AvailableSlots, finalSlots);
+        }
+
+        [TestCase(17)]
+        public void RemoveItemAvailableSlotsUnchanged(int finalSlots)
+        {
+            for (int x = 0; x < 3; x++)
+            {
+                testInventory.AddItem(testItem);
+            }
+
+            testInventory.TakeItem("Village key", out Item item);
+
+            // Check if the availableSlots remain unchanged at the end
+            Assert.AreEqual(testInventory.AvailableSlots, finalSlots);
+        }
+
+        [TestCase(17)]
+        public void AddItemAvailableSlotsDecrease_ItemsExist(int finalSlots)
+        {
+            for (int x = 0; x < 3; x++)
+            {
+                testInventory.AddItem(testItem);
+            }
+
+            List<Item> items = testInventory.ListAllItems();
+
+            // Check if the availableSlots changed depending on the items inserted
+            Assert.AreEqual(testInventory.AvailableSlots, finalSlots);
+            // Check if the list created contains the testItem we are looking for
+            Assert.IsTrue(items.Contains(testItem));
+        }
+
+        [TestCase(20)]
+        public void ResetAllSlotsRestored(int finalSlots)
+        {
+            for (int x = 0; x < 3; x++)
+            {
+                testInventory.AddItem(testItem);
+            }
+
+            testInventory.Reset();
+
+            // Check if the availableSlots go back to 20 when the Inventory is reseted
+            Assert.AreEqual(testInventory.AvailableSlots, finalSlots)
+        }
+
     }
 }
