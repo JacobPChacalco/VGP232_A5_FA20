2b40cd949a200f38598c0b324e2ebec54340d7a4
Jacob <jacobpchacalco@gmail.com>
2020-11-26 16:08:30 -0800

    Added NuGet packages NUnit, NUnit3TestAdapter & Microsoft.NET.Test.Sdk
    Created InventoryTest and CharacterTest classes

---
+++ Assignment5/Assignment5/InventoryTest.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using NuGet.Frameworks;
+using NUnit.Framework;
+
+namespace Assignment5
+{
+    public class InventoryTest
+    {
+    }
+}
